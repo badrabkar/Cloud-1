9bf5363f1a6ce4c12f6f51bec61f73be57bd4a9a
Badr Abkar <babkar@e2r8p15.1337.ma>
2025-07-02 18:56:46 +0100

    feat: sharing the public ip address of the vms with with ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,17 @@
 
+resource "local_file" "ansible_inventory" {
+  content = "[myhosts]\n"
+  filename = "${path.module}/../ansible/inventory.ini"
+}
+
 
 resource "google_compute_instance" "cloud" {
-	count 			 = 2
+  depends_on = [local_file.ansible_inventory]
+  count = 2
   name         = "vm-${count.index}"
   zone         = "us-central1-a"
   machine_type = "e2-micro"
-	
+
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2204-lts"
@@ -26,4 +32,14 @@ resource "google_compute_instance" "cloud" {
   metadata_startup_script = "echo hi > /test.txt"
   #   allow_stopping_for_update = true
 
+  provisioner "local-exec" { 
+    command = "echo $PUBLIC_IP >> $ANSIBLE_INVENTORY"
+    environment = {
+      PUBLIC_IP = self.network_interface[0].access_config[0].nat_ip
+      ANSIBLE_INVENTORY = local_file.ansible_inventory.filename
+    }
+  }
 }
+
+
+
