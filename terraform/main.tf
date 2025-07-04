aac9978e3fc325d030173b5f1de30dd7e33ab4e7
Badr Abkar <babkar@e2r9p15.1337.ma>
2025-07-04 09:19:09 +0100

    feat: adding some firewall rules to allow http and ssh connections

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,18 @@
 
 resource "local_file" "ansible_inventory" {
-  content = "[myhosts]\n"
+  content  = "[myhosts]\n"
   filename = "${path.module}/../ansible/inventory.ini"
 }
 
 
 resource "google_compute_instance" "cloud" {
-  depends_on = [local_file.ansible_inventory]
-  count = 2
+  depends_on   = [local_file.ansible_inventory]
+  count        = 2
   name         = "vm-${count.index}"
   zone         = "us-central1-a"
   machine_type = "e2-micro"
+
+  tags = ["vms"]
 
   boot_disk {
     initialize_params {
@@ -32,14 +34,44 @@ resource "google_compute_instance" "cloud" {
   metadata_startup_script = "echo hi > /test.txt"
   #   allow_stopping_for_update = true
 
-  provisioner "local-exec" { 
-    command = "echo $PUBLIC_IP >> $ANSIBLE_INVENTORY"
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo "$PUBLIC_IP ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> $ANSIBLE_INVENTORY
+    EOT
     environment = {
-      PUBLIC_IP = self.network_interface[0].access_config[0].nat_ip
+      PUBLIC_IP         = self.network_interface[0].access_config[0].nat_ip
       ANSIBLE_INVENTORY = local_file.ansible_inventory.filename
     }
   }
 }
+
+resource "google_compute_firewall" "rules" {
+  name = "my-firewall-rules"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = ["80", "443", "22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["vms"]
+}
+
+
+# resource "terraform_data" "known_hosts" {
+#   depends_on = [google_compute_instance.cloud]
+
+#   provisioner "local-exec" {
+#     # This command constructs a loop in bash to iterate over the IPs
+#     # and execute your desired command for each.
+#     command = <<-EOT
+#       IPS=${join(" ", [for instance in google_compute_instance.cloud : instance.network_interface[0].access_config[0].nat_ip])}
+      
+#       echo $IPS
+#     EOT
+#   }
+# }
 
 
 
